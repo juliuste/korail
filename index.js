b361a44817de02e6a4fb051f7446d5129a032d39
Julius Tens <mail@juliustens.eu>
2018-10-30 18:10:45 +0100

    add linter, index -> lib/index, code style, update dotfiles

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const stations = require('./lib/stations')
-const journeys = require('./lib/journeys')
-const journeyLeg = require('./lib/journeyLeg')
-
-module.exports = {stations, journeys, journeyLeg}
