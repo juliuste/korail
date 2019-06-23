9d13a3f566390839eba38dd28ce2a9510dd8dd39
Julius Tens <mail@juliustens.eu>
2019-06-23 13:39:20 +0200

    adapt to fpti-js: journeys

--- test.js
+++ test.js
@@ -12,10 +12,14 @@ const getStream = require('get-stream').array
 const korail = require('.')
 const pkg = require('./package.json')
 
+const when = DateTime.fromObject({ zone: 'Asia/Seoul', weekday: 4 }).plus({ weeks: 1, hours: 6 }).toJSDate() // next thursday, 06:00
+const isStationWithName = (s, name) => (s.type === 'station' && s.name === name)
+
 tape('korail fpti tests', async t => {
 	await t.doesNotReject(fptiTests.packageJson(pkg), 'valid package.json')
 	t.doesNotThrow(() => fptiTests.packageExports(korail, ['stations.all', 'journeys']), 'valid module exports')
 	t.doesNotThrow(() => fptiTests.stationsAllFeatures(korail.stations.all.features, []), 'valid stations.all features')
+	t.doesNotThrow(() => fptiTests.journeysFeatures(korail.journeys.features, ['when', 'departureAfter', 'results', 'interval', 'transfers']), 'valid journeys features')
 })
 
 tape('korail.stations.all', async t => {
@@ -35,69 +39,88 @@ tape('korail.stations.all', async t => {
 	t.ok(!!busan.major, 'busan major')
 })
 
-const isSeoul = (s) => (s.name === '서울')
-const isBusan = (s) => (s.name === '부산')
-const isMokpo = (s) => (s.name === '목포')
+tape('korail.journeys non-direct', async t => {
+	const mokpo = { id: '0041', type: 'station' }
+	const busan = '0020'
 
-tape('korail.journeys', async (t) => {
-	const when = DateTime.fromJSDate(new Date(), { zone: 'Asia/Seoul' }).plus({ days: 10 }).startOf('day').plus({ hours: 8 }).toJSDate()
+	const journeys = await korail.journeys(mokpo, busan, { when })
+	t.ok(journeys.length > 0, 'number of journeys')
+	for (let journey of journeys) {
+		t.doesNotThrow(() => validate(journey), 'valid fptf')
+		t.ok(isStationWithName(journey.legs[0].origin, '목포'), 'origin')
+		t.ok(isStationWithName(journey.legs[journey.legs.length - 1].destination, '부산'), 'destination')
+		t.ok(+new Date(journey.legs[0].departure) >= +when, 'departure')
 
-	// Seoul -> Busan, direct, KTX
-	const j1 = await korail.journeys('0001', { id: '0020', type: 'station' }, when, { direct: true, product: 'KTX' })
-	t.ok(j1.length >= 5, 'journeys length')
-	for (let journey of j1) {
-		validate(journey)
-		t.ok(journey.legs.length === 1, 'legs length')
-		const leg = journey.legs[0]
-		t.ok(isSeoul(leg.origin), 'leg origin')
-		t.ok(isBusan(leg.destination), 'leg destination')
-		t.ok(+new Date(leg.departure) - (+when) <= 12 * 60 * 60 * 1000, 'leg departure')
-		t.ok(leg.mode === 'train', 'leg mode')
-		t.ok(leg.operator === 'korail', 'leg operator')
-		t.ok(leg.line.type === 'line', 'leg line type')
-		t.ok(leg.line.id.length > 0, 'leg line id')
-		t.ok(leg.line.name.length > 0, 'leg line name')
-		t.ok(leg.line.product.id === '100', 'leg line product id')
-		t.ok(leg.line.product.name === 'KTX', 'leg line product name')
-		t.ok(leg.line.mode === 'train', 'leg line mode')
-		t.ok(leg.line.operator === 'korail', 'leg line operator')
-	}
-	const pricedJ1 = j1.filter(j => j.price)
-	t.ok(pricedJ1.length > 0, 'priced journeys')
-	for (let journey of pricedJ1) {
-		t.ok(journey.price.amount > 0, 'price amount')
-		t.ok(journey.price.currency === 'KRW', 'price currency')
-	}
-
-	// Mokpo -> Busan, direct
-	const j2 = await korail.journeys({ id: '0041', type: 'station' }, '0020', when, { direct: true })
-	t.ok(j2.length === 0, 'journeys length')
-
-	// Mokpo -> Busan, not direct
-	const j3 = await korail.journeys({ id: '0041', type: 'station' }, '0020', when)
-	t.ok(j3.length > 0, 'journeys length')
-	for (let journey of j3) {
-		validate(journey)
 		t.ok(journey.legs.length > 1, 'legs length')
 		for (let leg of journey.legs) {
-			t.ok(+new Date(leg.departure) - (+when) <= 24 * 60 * 60 * 1000, 'leg departure')
 			t.ok(leg.mode === 'train', 'leg mode')
-			t.ok(leg.operator === 'korail', 'leg operator')
-			t.ok(leg.line.type === 'line', 'leg line type')
-			t.ok(leg.line.id.length > 0, 'leg line id')
-			t.ok(leg.line.name.length > 0, 'leg line name')
+			t.ok(leg.operator.id === 'korail', 'leg operator')
+			t.doesNotThrow(() => validate(leg.line), 'valid fptf')
 			t.ok(leg.line.product.id.length > 0, 'leg line product id')
 			t.ok(leg.line.product.name.length > 0, 'leg line product name')
 			t.ok(leg.line.mode === 'train', 'leg line mode')
-			t.ok(leg.line.operator === 'korail', 'leg line operator')
+			t.ok(leg.line.operator.id === 'korail', 'leg line operator')
 		}
-		t.ok(isMokpo(journey.legs[0].origin), 'first leg origin')
-		t.ok(isBusan(journey.legs[journey.legs.length - 1].destination), 'last leg destination')
 	}
-	const pricedJ3 = j3.filter(j => j.price)
-	t.ok(pricedJ3.length === 0, 'priced journeys')
+	const journeysWithPrice = journeys.filter(j => j.price)
+	t.ok(journeysWithPrice.length === 0, 'number of journeys with price')
+	for (let journey of journeysWithPrice) {
+		t.ok(journey.price.amount > 0, 'price amount')
+		t.ok(journey.price.currency === 'KRW', 'price currency')
+	}
+})
 
-	t.end()
+tape('korail.journeys opt.product, opt.departureAfter', async t => {
+	const seoul = '0001'
+	const busan = '0020'
+
+	const journeys = await korail.journeys(seoul, busan, { departureAfter: when, transfers: 0, product: 'KTX' })
+	t.ok(journeys.length >= 5, 'number of journeys')
+	for (let journey of journeys) {
+		t.doesNotThrow(() => validate(journey), 'valid fptf')
+		t.ok(isStationWithName(journey.legs[0].origin, '서울'), 'origin')
+		t.ok(isStationWithName(journey.legs[journey.legs.length - 1].destination, '부산'), 'destination')
+		t.ok(+new Date(journey.legs[0].departure) >= +when, 'departure')
+		t.ok(journey.legs.some(leg => leg.line.product.id === '100' && leg.line.product.name === 'KTX'), 'KTX leg')
+	}
+})
+
+tape('korail.journeys opt.results', async t => {
+	const seoul = '0001'
+	const busan = '0020'
+
+	const journeys = await korail.journeys(seoul, busan, { when, results: 2 })
+	t.ok(journeys.length === 2, 'number of journeys')
+})
+
+tape('korail.journeys opt.transfers', async t => {
+	const mokpo = '0041'
+	const busan = { id: '0020', type: 'station' }
+
+	const journeysWithoutTransfer = await korail.journeys(mokpo, busan, { when, transfers: 0 })
+	t.ok(journeysWithoutTransfer.length === 0, 'number of journeys')
+
+	const journeysWithTransfer = await korail.journeys(mokpo, busan, { when, transfers: 2 })
+	t.ok(journeysWithTransfer.length > 0, 'number of journeys')
+	for (let journey of journeysWithTransfer) t.doesNotThrow(() => validate(journey), 'valid fptf')
+})
+
+tape('korail.journeys opt.interval', async t => {
+	const mokpo = '0041'
+	const busan = '0020'
+	const dayAfterWhen = DateTime.fromJSDate(when, { zone: 'Asia/Seoul' }).plus({ days: 1 }).toJSDate()
+
+	const journeysWithoutInterval = await korail.journeys(mokpo, busan, { when })
+	for (let journey of journeysWithoutInterval) t.doesNotThrow(() => validate(journey), 'valid fptf')
+	t.ok(journeysWithoutInterval.length > 0, 'precondition')
+	const journeysWithoutIntervalDayAfterWhen = journeysWithoutInterval.filter(journey => +new Date(journey.legs[0].departure) >= +dayAfterWhen)
+	t.ok(journeysWithoutIntervalDayAfterWhen.length === 0, 'number of journeys')
+
+	const journeysWithInterval = await korail.journeys(mokpo, busan, { when, interval: 30 * 60 }) // journeys for the next 30h
+	for (let journey of journeysWithInterval) t.doesNotThrow(() => validate(journey), 'valid fptf')
+	t.ok(journeysWithInterval.length > 0, 'precondition')
+	const journeysWithIntervalDayAfterWhen = journeysWithInterval.filter(journey => +new Date(journey.legs[0].departure) >= +dayAfterWhen)
+	t.ok(journeysWithIntervalDayAfterWhen.length > 0, 'number of journeys')
 })
 
 tape('korail.journeyLeg', async (t) => {
@@ -122,6 +145,4 @@ tape('korail.journeyLeg', async (t) => {
 			t.ok(+new Date(stopover.arrival) <= +new Date(stopover.departure), 'arrival before departure')
 		}
 	}
-
-	t.end()
 })
